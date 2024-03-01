8bb30c2f6c2ae46da6ded41d26090ccf4b4fac35
Vova778 <voha.kopovay@gmail.com>
2024-03-01 22:14:55 +0200

    logout added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Test Name',
+            'email' => 'test@jura.com',
+        ]);
         User::factory(300)->create();
 
         $users = User::all()->shuffle();
